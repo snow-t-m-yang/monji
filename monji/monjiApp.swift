481825bf92b0e4ec8b4e57d628e3a51f0d050c24
Snow Yáng <ytm199891@gmail.com>
2025-01-20 19:03:24 -0600

    Add firebase

--- monji/monjiApp.swift
+++ monji/monjiApp.swift
@@ -5,10 +5,26 @@
 //  Created by snow on 2025/1/20.
 //
 
+import FirebaseCore
 import SwiftUI
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication
+            .LaunchOptionsKey: Any]? = nil
+    ) -> Bool {
+        FirebaseApp.configure()
+
+        return true
+    }
+}
 
 @main
 struct monjiApp: App {
+    // register app delegate for Firebase setup
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
     var body: some Scene {
         WindowGroup {
             ContentView()

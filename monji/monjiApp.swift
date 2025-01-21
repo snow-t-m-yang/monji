84adf8eb2108580b94d331f55a69b710641e8fb4
Snow Yáng <ytm199891@gmail.com>
2025-01-20 19:24:53 -0600

    Remove app delegate

--- monji/monjiApp.swift
+++ monji/monjiApp.swift
@@ -8,22 +8,12 @@
 import FirebaseCore
 import SwiftUI
 
-class AppDelegate: NSObject, UIApplicationDelegate {
-    func application(
-        _ application: UIApplication,
-        didFinishLaunchingWithOptions launchOptions: [UIApplication
-            .LaunchOptionsKey: Any]? = nil
-    ) -> Bool {
-        FirebaseApp.configure()
-
-        return true
-    }
-}
-
 @main
 struct monjiApp: App {
-    // register app delegate for Firebase setup
-    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    init() {
+        FirebaseApp.configure()
+        print("configured firebase")
+    }
 
     var body: some Scene {
         WindowGroup {
